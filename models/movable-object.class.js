44dc117d12cc3e69b9bb3dfe564fe2c78e24dbc2
Caro <140181592+CaroWillers@users.noreply.github.com>
2024-06-12 09:38:15 +0200

    first commit

---
+++ models/movable-object.class.js
@@ -0,0 +1,30 @@
+class MovableObject {
+    x = 120;
+    y = 300;
+    img;  
+    width = 150;
+    height = 150;
+    imageCache = {};
+    currentImage = 0;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    loadImages(arr) {   
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+        
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
+    }
+}
---
+++ models/world.class.js
@@ -0,0 +1,51 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud()    
+    ];
+    backgroundObjects = [   
+        new BackgroundObject(' img/5_background/layers/air.png',0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png',0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png',0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png',0)       
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) { 
+        this.canvas = canvas;        
+        this.ctx = canvas.getContext('2d');
+        this.draw(); 
+    }
+
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        
+        this.addObjectToMap(this.backgroundObjects);
+        this.addToMap(this.character);
+        this.addObjectToMap(this.clouds);
+        this.addObjectToMap(this.enemies);
+       
+
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+    })
+}
+
+addObjectToMap(objects) {
+    objects.forEach(object => {
+        this.addToMap(object);
+    });
+}
+
+addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+}
+}
