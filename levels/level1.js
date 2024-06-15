2909e80e8ff3386038ac130953bed2a8f0e7a7b4
Caro <140181592+CaroWillers@users.noreply.github.com>
2024-06-15 12:20:13 +0200

    Optimierungen

--- levels/level1.js
+++ levels/level1.js
@@ -1,53 +1,40 @@
-const level1 = new Level();
-const enemies = [
+const level1 = new Level(
+    [
     new Chicken(),
     new Chicken(),
     new Chicken(),
-];
+],
+[
+    new Cloud()
+],
+[
+    new BackgroundObject('img/5_background/layers/air.png',-719),
+    new BackgroundObject('img/5_background/layers/4_clouds/2.png',-719),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png',-719),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png',-719),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png',-719),
+    new BackgroundObject('img/5_background/layers/air.png',0),
+    new BackgroundObject('img/5_background/layers/4_clouds/1.png',0),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png',0),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png',0),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png',0),
+    new BackgroundObject('img/5_background/layers/air.png',719),
+    new BackgroundObject('img/5_background/layers/4_clouds/2.png',719),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png',719),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png',719),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png',719),
+    new BackgroundObject('img/5_background/layers/air.png',719*2),
+    new BackgroundObject('img/5_background/layers/4_clouds/1.png',719*2),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png',719*2),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png',719*2),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png',719*2),
+    new BackgroundObject('img/5_background/layers/air.png',719*3),
+    new BackgroundObject('img/5_background/layers/4_clouds/2.png',719*3),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png',719*3),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png',719*3),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png',719*3),
+]
+);
 
-const clouds = [
-    new Cloud(),
-];
-
-const backgroundObjects = [];  
- 
-
-function generateInitialBackgroundObjects(level) {
-    const segmentWidth = 719;
-    const initialRepeats = 10; // Initial number of times to repeat the background
-
-    for (let i = 0; i < initialRepeats; i++) {
-        addBackgroundSegment(level, i * segmentWidth * 2); // *2 to account for two sets of layers
-    }
-}
-
-function addBackgroundSegment(level, offsetX) {
-    const layers1 = [
-        'img/5_background/layers/air.png',
-        'img/5_background/layers/3_third_layer/1.png',
-        'img/5_background/layers/2_second_layer/1.png',
-        'img/5_background/layers/1_first_layer/1.png'
-    ];
-
-    const layers2 = [
-        'img/5_background/layers/air.png',
-        'img/5_background/layers/3_third_layer/2.png',
-        'img/5_background/layers/2_second_layer/2.png',
-        'img/5_background/layers/1_first_layer/2.png'
-    ];
-
-    layers1.forEach(layer => {
-        level.backgroundObjects.push(new BackgroundObject(layer, offsetX));
-    });
-
-    offsetX += 719; // Move to the next segment for the second set of layers
-
-    layers2.forEach(layer => {
-        level.backgroundObjects.push(new BackgroundObject(layer, offsetX));
-    });
-}
-
-// Generate initial background objects for level1
-generateInitialBackgroundObjects(level1);
 
     
--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,6 @@
 class World {
     character = new Character();
-    enemies = level1.enemies;
-    clouds = level1.clouds;
-    backgroundObjects = level1.backgroundObjects;
+    level = level1;
     canvas;
     ctx;
     keyboard;
@@ -11,7 +9,7 @@ class World {
     constructor(canvas, keyboard) { 
         this.canvas = canvas;    
         this.keyboard = keyboard;    
-        this.ctx = canvas.getContext('2d');
+        this.ctx = canvas.getContext('2d'); 
         this.character.world = this;
         this.draw(); 
         this.setWorld();    
@@ -36,12 +34,13 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
  
-        this.addObjectToMap(this.backgroundObjects); 
+        this.addObjectsToMap(this.level.backgroundObjects); 
+
         this.addToMap(this.character);
-        this.addObjectToMap(this.clouds);
-        this.addObjectToMap(this.enemies);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
+
         this.ctx.translate(-this.camera_x, 0);
-        this.checkBackgroundExtension();
 
         let self = this;
         requestAnimationFrame(function() {
@@ -49,18 +48,11 @@ class World {
     })
 }
 
-checkBackgroundExtension() {
-    const segmentWidth = 719;
-    const buffer = segmentWidth * 2; // Buffer distance to add new segments before reaching the edge
 
-    if (this.character.x + this.canvas.width > this.backgroundObjects[this.backgroundObjects.length - 1].x + buffer) {
-        this.generateBackgroundObjects();
-    }
-}
 
-addObjectToMap(objects) {
-    objects.forEach(object => {
-        this.addToMap(object);
+addObjectsToMap(objects) {
+    objects.forEach(o => {
+        this.addToMap(o);
     });
 }
 
